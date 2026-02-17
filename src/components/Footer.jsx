13e53aad6385ebe22b3c1555144277303902ef88
Nolan Shaffer <nshaff3r@gmail.com>
2026-02-16 22:01:14 -0600

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Credits() {
         <div className="w-[90%] md:w-[80%] mx-auto">
             <div className="w-full h-[3px] bg-black rounded-md"></div>
             <div className="py-5">
-                <p className="text-[16px] mb-2">
+                {/* <p className="text-[16px] mb-2">
                     Unless otherwise noted, photos courtesy of BOB (Robert Chase
                     Heishman) via the Neubauer Collegium.
-                </p>
+                </p> */}
                 <p className="text-[16px]">
                     Find the code for this interactive on{' '}
-                    <a href="https://github.com/chicagomaroon/data-visualizations/tree/main/2026/zoya-cherkassky-nnadi-src/">
+                    <a href="https://github.com/nshaff3r/battle-of-the-djs">
                         GitHub
                     </a>
                     .
